5012207fa596a5189870ccb5689180938728ca11
Sharad Katre <sharad.katre@mnscorp.net>
2025-04-30 01:35:48 +0530

    Images path update

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,7 +6,7 @@ export default function AboutMe(_: AboutMeProps) {
   return (
     <section id="AboutMe" className="about--section">
       <div className="about--section--img">
-        <img src="src/assets/images/about-me.png" alt="About Me" />
+        <img src="./src/assets/images/about-me.png" alt="About Me" />
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
